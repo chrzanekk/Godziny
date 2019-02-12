e88b04ebd6772402c228d3a9510aecfc603e7275
Konrad <chrzanekk@gmail.com>
2019-02-12 17:46:39 +0100

    Update liczenie_godzin.py
    Added coment in main file. Little descrpition what i want to write.

--- liczenie_godzin.py
+++ liczenie_godzin.py
@@ -1,4 +1,10 @@
-def godziny(godz_pocz=None,godz_konc=None):
+#simple script to count work hours
+#now count only real time of work.
+#in future i want to add some impruvements like date input with hours
+#final job is to count work hours with additional hours and add 50% and 100% extra paymnet
+
+
+def godziny(godz_pocz=None,godz_konc=None): 
     godz_pocz = int(input("Godzina rozpoczęcia pracy: "))
     godz_konc = int(input("Godzina zakończenia pracy: "))
     ilosc = godz_konc - godz_pocz
