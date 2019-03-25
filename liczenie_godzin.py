6bcd2754e2edfe294ea3733729423154694a3f36
Konrad <chrzanekk@gmail.com>
2019-03-25 12:53:44 +0100

    Some changes: 1. new function for date input 2. little changes in clear code 3. added class

--- liczenie_godzin.py
+++ liczenie_godzin.py
@@ -5,25 +5,45 @@
 
 import calendar
 
+class setDateTime:
+    def __init__(self, day, month, year, hour, minute):
+        self.day = day
+        self.month = month
+        self.year = year
+        self.hour = hour
+        self.minute = minute
 
-#simple function that convert inputed date to day of week
-def input_date():
-    d = int(input("Input day/Podaj dzień: "))
-    m = int(input("Input month/Podaj miesiac: "))
-    y = int(input("Input year/Podaj rok: "))
-    day_of_week = calendar.weekday(y, m, d)
-    day_of_week_dict = {0: "Monday/Poniedzialek",
-                        1: "Tuesday/Wtorek",
-                        2: "Wednesday/Środa",
-                        3: "Thursday/Czwartek",
-                        4: "Friday/Piątek",
-                        5: "Saturday/Sobota",
-                        6: "Sunday/Niedziela"}
-    for k in day_of_week_dict:
-        if k == day_of_week:
-            print(day_of_week_dict[k])
-            break
-    return day_of_week
+    def setDate(self):
+        InputDate = input("Set date in format DD/MM/YYYY: ")
+        day, month, year = map(int, InputDate.split("/"))
+        Date1 = {"Day: ": day, "Month: ": month, "Year: ": year}
+        return Date1
+
+    def setTime(self):
+        InputTime = input("Set time in format HH:MM: ")
+        hour, minutes = map(int, InputTime.split(":"))
+        Time1 = {"Hour: ": hour, "Minutes: ": minutes}
+        return Time1
+
+
+# #simple function that convert inputed date to day of week
+# def input_date():
+#     d = int(input("Input day/Podaj dzień: "))
+#     m = int(input("Input month/Podaj miesiac: "))
+#     y = int(input("Input year/Podaj rok: "))
+#     day_of_week = calendar.weekday(y, m, d)
+#     day_of_week_dict = {0: "Monday/Poniedzialek",
+#                         1: "Tuesday/Wtorek",
+#                         2: "Wednesday/Środa",
+#                         3: "Thursday/Czwartek",
+#                         4: "Friday/Piątek",
+#                         5: "Saturday/Sobota",
+#                         6: "Sunday/Niedziela"}
+#     for k in day_of_week_dict:
+#         if k == day_of_week:
+#             print(day_of_week_dict[k])
+#             break
+#     return day_of_week
 
 
 #simple function that convert inputed date to day of week
