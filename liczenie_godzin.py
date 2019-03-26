660b353a865b61b3e0d0bbe252e7bbdbcdcd0a5b
Konrad <chrzanekk@gmail.com>
2019-03-26 08:20:33 +0100

    Some description added. And first lines of input star and finish work.

--- liczenie_godzin.py
+++ liczenie_godzin.py
@@ -7,14 +7,15 @@ import calendar
 import datetime
 
 
-def date_input():
+def date_time_input():
     try:
-        date_entry = input("Input date in DD/MM/YYYY format: ")
-        day, month, year = map(int, date_entry.split("/"))
-        date = [day, month, year]
-        return date
+        date_entry = input("Input date and time in DD/MM/YYYY/HH/MM format: ")
+        day, month, year, hour, minutes = map(int, date_entry.split("/"))
+        date_time = [day, month, year, hour, minutes]
+        return date_time
     except ValueError:
         print("Incorrect format.")
+
 
 
 # #simple function that convert inputed date to day of week
@@ -29,23 +30,45 @@ def check_date(y, m, d):
                         6: "Sunday/Niedziela"}
     for k in day_of_week_dict:
         if k == day_of_week:
-            print(day_of_week_dict[k])
+            # print(day_of_week_dict[k])
             break
-    return day_of_week
+    return day_of_week_dict[k]
 
 
-#simple function that convert inputed date to day of week
-# def hours():
-#     start_hour = int(input("Input start hour. / Godzina rozpoczęcia pracy: "))
-#     end_hour = int(input("Input end hour. / Godzina zakonczenia pracy: "))
-#     count = godz_konc - godz_pocz
-#     print("Work time is / Czas pracy to:", count, "hours / godzin. ")
-#     return ilosc
+print("HOUR COUNTER 2019 - by K. Chrzanowski")
+print("-------------------------------------")
+print("Simple script to count extra hours of work")
+print("depending by day and hours. Script check what ")
+print("day of week was from entered date and time and")
+print("then use appriopriate function.")
+print("Key: ")
+print("Mon - Fri: 0-8h = +0%, 9-10h = +50%, >10h = +100%")
+print("Saturday: all day + 50%")
+print("Sunday: all day +100%")
+print("-------------------------------------")
+print("Start work:")
+date1 = date_time_input()
+try:
+    what_day1 = check_date(date1[2], date1[1], date1[0])
+    # print(what_day1)
+except TypeError:
+    print("Check date/format and try again.")
+print("End work:")
+date2 = date_time_input()
+try:
+    what_day2 = check_date(date2[2], date2[1], date2[0])
+    # print(what_day2)
+except TypeError:
+    print("Check date/format and try again.")
 
+print("Start work at: ", what_day1)
+print("End work at: ", what_day2)
 
-
-
-
+#code for counting hours from date and time entered above.
+start_hour = date1[3]
+start_min = date1[4]
+end_hour = date2[3]
+end_min = date2[4]
 
 
 
