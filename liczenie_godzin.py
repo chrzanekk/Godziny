a5af4ddd8193ff7a5e98460da00afb64436f42f2
Konrad <chrzanekk@gmail.com>
2019-03-27 09:09:57 +0100

    Added validation of date and time. Added some of try/except block to handel exception when user input wrong data at the begining.

--- liczenie_godzin.py
+++ liczenie_godzin.py
@@ -1,7 +1,7 @@
 #simple script to count work hours
-#now count only real time of work.
 #you can input date. i use it to check what day of week it is and coresponding to that day cout and addition to payment
-#final job is to count work hours with additional hours and add 50% and 100% extra payment
+#final versioncount work hours with additional hours and add 50% and 100% extra payment.
+
 
 import calendar
 import datetime
@@ -27,7 +27,6 @@ def date_time_input():
 
     except ValueError:
         print("Incorrect format.")
-
 
 
 # #simple function that convert inputed date to day of week
